a769f437e4180172f3a2d8bee103d717a7c47eb4
John Ruffer <jqruffer@vt.edu>
2012-11-30 09:34:17 -0500

    got the pointer usage to work WOOO

--- oscillators_w_jack/src/osc_test.cpp
+++ oscillators_w_jack/src/osc_test.cpp
@@ -6,18 +6,20 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include "Oscillator.h"
 #include "SineOscillator.h"
+#include "FmOscillator.h"
 
 typedef jack_default_audio_sample_t sample_t;
-SineOscillator osc;
 jack_port_t * outputPort;
 
 int process( jack_nframes_t nframes, void * args ) {
 	sample_t * buffer = (sample_t *) jack_port_get_buffer( outputPort, nframes );
-	
+	Oscillator * osc = (Oscillator *) args;
+
 	jack_nframes_t i;
 	for( i = 0; i < nframes; i++ ) {
-		buffer[i] = osc.nextSample();
+		buffer[i] = osc->nextSample();
 	}
 	
 	return 0;
@@ -32,15 +34,16 @@ int main( int argc, char ** argv ) {
 		fprintf (stderr, "jack server not running?\n");
 		return 1;
 	}
-	
-	jack_set_process_callback( client, process, 0 );
+
+	Oscillator * osc = new FmOscillator( 48000, 440.0f, 1.2, 2.0 );	
+
+	jack_set_process_callback( client, process, osc );
 	outputPort = jack_port_register( client, "Hz", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0 );
 
 	int sampleRate = jack_get_sample_rate( client );
 	float frequency = 440.0f;
-
-	osc.setSampleRate( sampleRate );
-	osc.setFrequency( frequency );
+	
+	osc->setSampleRate( sampleRate );
 
 	if ( jack_activate( client ) ) {
 		fprintf (stderr, "cannot activate client");
